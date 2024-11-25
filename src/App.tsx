7c8ae0e24147fe2fb9487622c769325012e4395f
electron271 <66094410+electron271@users.noreply.github.com>
2024-11-25 00:35:20 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ const sigmaStyle = { height: "100vh", width: "100vw" };
 
 // colormap function
 const cmap = colormap({
-  colormap: 'warm',
+  colormap: 'bluered',
   nshades: 101,
   format: 'hex',
   alpha: [0, 1],
