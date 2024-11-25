70dbb60ad8c4b0658097a5e20befe5d6542de563
electron271 <66094410+electron271@users.noreply.github.com>
2024-11-24 19:43:39 -0600

    add layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,48 @@
 import './index.css'
 
-import { useEffect } from "react";
+import { FC, useEffect } from "react";
 import Graph from "graphology";
 import { parse } from 'graphology-gexf';
 import { SigmaContainer, useLoadGraph } from "@react-sigma/core";
 import "@react-sigma/core/lib/react-sigma.min.css";
+import { useWorkerLayoutForceAtlas2 } from "@react-sigma/layout-forceatlas2";
+import { useLayoutCircular } from '@react-sigma/layout-circular';
 
 //const sigmaStyle = { height: "500px", width: "500px" };
 // use whole screen
 const sigmaStyle = { height: "100vh", width: "100vw" };
 
+const Fa2: FC = () => {
+  const { start, kill } = useWorkerLayoutForceAtlas2({ settings: { 
+    slowDown: 10,
+    gravity: 0.1,
+    scalingRatio: 2,
+    strongGravityMode: false,
+    outboundAttractionDistribution: false,
+    linLogMode: false,
+    adjustSizes: false,
+    barnesHutOptimize: false,
+    barnesHutTheta: 0.5,
+    edgeWeightInfluence: 1.5,
+   }});
+
+  useEffect(() => {
+    // start FA2
+    start();
+
+    // Kill FA2 on unmount
+    return () => {
+      kill();
+    };
+  }, [start, kill]);
+
+  return null;
+};
+
 // Component that load the graph
 export const LoadGraph = () => {
   const loadGraph = useLoadGraph();
+  const { positions, assign } = useLayoutCircular();
 
   useEffect(() => {
     const gexfUrl = import.meta.env.VITE_GEXF_URL;
@@ -22,15 +52,17 @@ export const LoadGraph = () => {
       .then((gexf) => {
         const graph = parse(Graph, gexf);
 
-        // add random layout
+        // set 0,0 for all nodes
         graph.forEachNode((node) => {
-          graph.setNodeAttribute(node, "x", Math.random());
-          graph.setNodeAttribute(node, "y", Math.random());
+          graph.setNodeAttribute(node, 'x', 0);
+          graph.setNodeAttribute(node, 'y', 0);
         });
 
         loadGraph(graph);
+        assign();
+        console.log(positions());
       });
-  }, [loadGraph]);
+  }, [loadGraph, assign, positions]);
 
   return null;
 };
@@ -40,6 +72,7 @@ export const DisplayGraph = () => {
   return (
     <SigmaContainer style={sigmaStyle}>
       <LoadGraph />
+      <Fa2 />
     </SigmaContainer>
   );
 };
