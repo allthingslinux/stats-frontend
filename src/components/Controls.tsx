d5a63c3bffb30f5a92051ec43452ad46628afd1a
electron271 <66094410+electron271@users.noreply.github.com>
2024-11-25 19:15:09 -0600

    Update Controls.tsx

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -21,7 +21,7 @@ export const Complete: FC<{ _style?: CSSProperties }> = ({ _style }) => {
       <ControlsContainer position={"bottom-right"} className="md:w-2/6 w-full">
         <div className="flex flex-row-reverse align-middle">
           <SearchControl className="w-2/6 px-2" />
-          </ZoomControl>
+          <ZoomControl />
           <FullScreenControl /> 
         </div>
         {/* <LayoutsControl /> */}
